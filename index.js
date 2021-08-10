dfda9891c3bdcb331d28f0f3b674bfe52a67ea9e
Julie Ann Meng <JulieAnnMeng@gmail.com>
2021-08-10 09:11:25 -0400

    commit tree 8d8086be7828f0370802d387cef0bd049de3e4d7

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = () => 2000 / 100;
+//The test does not add in any parameters for the test to pass. I would write it as follows
+// const divide = (parameter1, parameter2) => parameter1 / parameter2;
+const square = x => x ** 2;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
